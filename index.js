c1235ec9e4b342967227de54dedd893bdc97ef04
sabbat10 <sabbatlumpatshia99@gmail.com>
2025-10-30 11:44:06 +0100

    feat: add depot

--- index.js
+++ index.js
@@ -23,5 +23,40 @@ function afficherSoldeCompte(unCompte) {
   console.log(`💰 Solde actuel de ${unCompte.nom} : ${unCompte.solde}$ `);
 }
 
+// depot d'argent
+
+function depot(montant) {
+  // 1️⃣ Vérifier que le montant est valide
+  if (montant <= 0 || isNaN(montant)) {
+    console.log("⚠️ monntant invalide !");
+    return;
+  }
+  // 2️⃣ Ajouter le montant au solde du compte
+  compte.solde += montant;
+
+  // 3️⃣ Enregistrer la transaction dans l’historique
+  compte.transactions.push({
+    type: "depot",
+    montant: montant,
+    date: new Date().toDateString(),
+  });
+
+  // 4️⃣ Confirmer à l’utilisateur
+  console.log(`✅ Dépôt effectué avec succès ! : +${montant}$`);
+  console.log(`💰 Nouveau solde : ${compte.solde}$`);
+}
+
 afficherSoldeCompte(compte);
-afficherSoldeCompte(compte2);
+// afficherSoldeCompte(compte2);
+
+depot(500);
+depot(300);
+depot(-200);
+
+for (let comptes of compte.transactions) {
+  console.log(
+    `Type: ${comptes.type}, Montant: ${comptes.montant}$, Date: ${comptes.date}`
+  );
+}
+
+// console.log(compte.transactions);
